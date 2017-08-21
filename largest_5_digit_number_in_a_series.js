c10f9a9ba7d7f3eb34cee53d99ddebbe99f70761
dustin <dbyers84@gmail.com>
2017-08-21 13:34:11 -0700

    solved

--- largest_5_digit_number_in_a_series.js
+++ largest_5_digit_number_in_a_series.js
@@ -3,18 +3,18 @@
 function solution(digits) {
 	let highestCurrentNum = 0;
 
-	// if (digits.length <= 0) {
-	// 	return digits;
-	// }
+	if (digits.length <= 5) {
+		return parseInt(digits);
+	}
 
 	for (let i = 0; i < digits.length; i++) {
-		let tempNum = Number(digits.slice(i, i + 5).concat(''));
-		console.log('tempNum');
+		let tempNum = parseInt(digits.slice(i, i + 5));
+		console.log('tempNum', tempNum);
 		if (tempNum > highestCurrentNum) {
-			let highestCurrentNum = tempNum;
+			highestCurrentNum = tempNum;
+			console.log('highestCurrentNum', highestCurrentNum);
 		}
 	}
 	return highestCurrentNum;
 }
-
-console.log(solution(12345678999));
+console.log('ANSWER', solution('29530267458234'));
